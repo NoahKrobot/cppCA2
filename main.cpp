598660bae399bb60bb5cbd67374e652182b25469
NoahKrobot <D00259927@student.dkit.ie>
2024-04-28 17:34:28 +0100

    reading from file fixed

--- main.cpp
+++ main.cpp
@@ -99,19 +99,68 @@ int main() {
     vector<Bug *> bug_vectorSmallBugs;
     vector<Bug *> bug_vector;
 
+//
+//    Crawler *craw = new Crawler('C', 1, 1, 1, 1, 3, "ALIVE");
+//    bug_vector.push_back(craw);
+//
+//    El_Diagonal *pElDiagonal = new El_Diagonal('D', 3, 1, 5, 5, 3, "ALIVE");
+//    bug_vector.push_back(pElDiagonal);
+//
+//    Hopper *hopp = new Hopper('H', 2, 3, 6, 4, 2, 2, "ALIVE");
+//    bug_vector.push_back(hopp);
+//    Crawler *craw2 = new Crawler('C', 4, 8, 7, 1, 3, "ALIVE");
+//    bug_vector.push_back(craw2);
+//    Crawler *craw3 = new Crawler('C', 5, 8, 7, 1, 3, "ALIVE");
+//    bug_vector.push_back(craw3);
 
-    Crawler *craw = new Crawler('C', 1, 1, 1, 1, 3, "ALIVE");
-    bug_vector.push_back(craw);
 
-    El_Diagonal *pElDiagonal = new El_Diagonal('D', 3, 1, 5, 5, 3, "ALIVE");
-    bug_vector.push_back(pElDiagonal);
+    ifstream file("../bugsFile.txt");
 
-    Hopper *hopp = new Hopper('H', 2, 3, 6, 4, 2, 2, "ALIVE");
-    bug_vector.push_back(hopp);
-    Crawler *craw2 = new Crawler('C', 4, 8, 7, 1, 3, "ALIVE");
-    bug_vector.push_back(craw2);
-    Crawler *craw3 = new Crawler('C', 5, 8, 7, 1, 3, "ALIVE");
-    bug_vector.push_back(craw3);
+    if (!file.is_open()) {
+        cout << "File cant be opened" << endl;
+    } else {
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            char delimiter = ';';
+            string partOfTheLine;
+            vector<string> bugsString;
+
+            while (getline(ss, partOfTheLine, delimiter)) {
+                bugsString.push_back(partOfTheLine);
+            }
+
+            int id = stoi(bugsString.at(1));
+            int xValue = stoi(bugsString.at(2));
+            int yValue = stoi(bugsString.at(3));
+            int direction = stoi(bugsString.at(4));
+            int size = stoi(bugsString.at(5));
+
+            if (bugsString.at(0) == "C") {
+                Crawler *craw = new Crawler('C', id, xValue, yValue, direction, size, "ALIVE");
+                bug_vector.push_back(craw);
+            } else if (bugsString.at(0) == "H") {
+                int hopLength = stoi(bugsString.at(6));
+                Hopper *hopp = new Hopper('H', id, xValue, yValue, direction, size, hopLength, "ALIVE");
+                bug_vector.push_back(hopp);
+            } else if (bugsString.at(0) == "D") {
+                El_Diagonal *eldi = new El_Diagonal('D', id, xValue, yValue, direction, size, "ALIVE");
+                bug_vector.push_back(eldi);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     //tiles vector
@@ -437,99 +486,12 @@ int main() {
         }
         window.display();
     }
-
-//    int choice = -1;
-//    do {
-//
-//        cout << "Menu Items" << endl;
-//
-//
-//        cout << "1. Initialize Bug Board (load data from file)" << endl;
-//        cout << "2. Display all Bugs" << endl;
-//        cout << "3. Find a Bug (given an id)" << endl;
-//        cout << "4. Tap the Bug Board (causes move all, then fight/eat)" << endl;
-//        cout << "5. Display Life History of all Bugs (path taken)" << endl;
-//        cout << "6. Display all Cells listing their Bugs" << endl;
-//        cout << "7. Run simulation (generates a Tap every second)" << endl;
-//        cout << "8. Exit (write Life History of all Bugs to file)" << endl;
-//        cout << "Enter your choice: ";
-//        cin >> choice;
-//
-//        switch (choice) {
-//            case 1:
-//                initializeBugBoard();
-//                break;
-//            case 2:
-//                (new Board())->displayAllBugs(bug_vector, 3);
-//                break;
-//            case 3:
-//                (new Board())->findBugById(bug_vector, 3);
-//                break;
-//            case 4:
-//                (new Board())->tapBugBoard(bug_vector, bug_vector);
-//                break;
-//            case 5:
-//                (new  Board())->displayLifeHistory(bug_vector, 3);
-//                break;
-//            case 6:
-//                displayAllCells();
-//                break;
-//            case 7:
-//                runSimulation();
-//                break;
-//            case 8:
-//                (new  Board())->exitProgram(bug_vector, 3);
-//                break;
-//            default:
-//                cout << "Invalid choice. Please enter a number between 1 and 8." << endl;
-//        }
-//    } while (choice != 8);
     return 0;
 }
 
 
 void readBugsFromFile(vector<Bug *> &bug_vector, const string &fileName) {
-    ifstream file(fileName);
 
-    if (!file.is_open()) {
-        cout << "File cant be opened" << endl;
-    } else {
-
-
-        while (!file.eof()) {
-            string line;
-            char delimiter = ';';
-            stringstream ss(line);
-            string partOfTheLine;
-            vector<string> bugsString;
-
-            getline(ss, partOfTheLine, delimiter);
-
-            while (getline(ss, partOfTheLine, delimiter)) {
-                bugsString.push_back(partOfTheLine);
-            }
-
-
-            int id = stoi(bugsString.at(1));
-            int xValue = stoi(bugsString.at(2));
-            int yValue = stoi(bugsString.at(3));
-            int direction = stoi(bugsString.at(4));
-            int size = stoi(bugsString.at(5));
-
-            //bug_vector.push_back(New Crawler (...))
-//            if (bugsString.at(0) == "C") {
-//                Crawler *craw = new Crawler('C', id, xValue, yValue, direction, size);
-//                bug_vector.push_back(craw);
-//            } else if (bugsString.at(0) == "H") {
-//                int hopLength = stoi(bugsString.at(6));
-//                Hopper *hopp = new Hopper('H', id, xValue, yValue, direction, size, hopLength);
-//                bug_vector.push_back(hopp);
-//            } else if (bugsString.at(0) == "E") {
-//                El_Diagonal *eldi = new El_Diagonal('E', id, xValue, yValue, direction, size);
-//                bug_vector.push_back(eldi);
-//            }
-        }
-    }
 }
 
 
